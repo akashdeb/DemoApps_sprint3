080d92288291da3ab9b65e8c84e05d9e13716436
akashdeb <akashdeb87@gmail.com>
2024-03-19 10:37:43 +0530

    Demo commit

--- demoApps/src/test/java/demoClass/DemoTest.java
+++ demoApps/src/test/java/demoClass/DemoTest.java
@@ -5,6 +5,9 @@ public class DemoTest {
 	public static void main(String[] args) {
 		
 		System.out.println("this is from demo Class");
+		
+		System.out.println("this is from Local Repo");
+
 
 	}
 
